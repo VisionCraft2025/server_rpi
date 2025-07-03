24580b98bc04c9cfc865db0ffc17518b856ac092
MIN60 <whitedaisy12759@gmail.com>
2025-07-03 03:07:27 +0100

    Style/#5: Set stream ratio to 16:9

--- rtsp_server/main.c
+++ rtsp_server/main.c
@@ -12,21 +12,11 @@ int main(int argc, char *argv[]) {
     GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
 
     // 파이프라인 생성 (라즈베리파이 카메라 → H.264 인코딩 → RTP 패킷화)
-    // const gchar *launch_desc =
-    //     "( libcamerasrc ! video/x-raw,width=640,height=480,framerate=30/1 ! "
-    //     "videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! "
-    //     "rtph264pay name=pay0 pt=96 )";
-
-    // const gchar *launch_desc =
-    //     "( libcamerasrc ! video/x-raw,width=640,height=480,framerate=15/1 ! "
-    //     "videoconvert ! x264enc tune=zerolatency bitrate=1000 speed-preset=ultrafast key-int-max=15 ! "
-    //     "rtph264pay name=pay0 pt=96 )";
-        
-
     const gchar *launch_desc =
-        "( libcamerasrc ! video/x-raw,width=640,height=480,framerate=15/1 ! "
+        "( libcamerasrc ! video/x-raw,width=1280,height=720,framerate=15/1 ! "
         "videoconvert ! x264enc tune=zerolatency bitrate=1000 speed-preset=ultrafast key-int-max=15 ! "
         "rtph264pay name=pay0 pt=96 )";
+        
     // 미디어 팩토리 생성 및 파이프라인 연결
     GstRTSPMediaFactory *factory = gst_rtsp_media_factory_new();
     gst_rtsp_media_factory_set_launch(factory, launch_desc);
